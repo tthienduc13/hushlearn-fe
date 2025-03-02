cceb03d8c9c81134cb11f6b82f093fc2f7ddeb0f
Duc Nguyen <ducnltdev@gmail.com>
2025-03-02 17:43:13 +0700

    feat(ui): Initialize Shadcn ui and theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from '@/components/ui/button';
+import { H1, InlineCode } from '@/components/ui/typography';
 import Image from 'next/image';
 
 export default function Home() {
+  const abc = 'abc';
   return (
     <div className="flex flex-col">
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
+        <Button variant={'destructive'}>asdf</Button>
+        <H1>{abc}</H1>
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -14,11 +19,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left">
           <li className="mb-2">
-            Get started by editing{' '}
-            <code className="rounded bg-black/[.05] px-1 py-0.5 font-semibold dark:bg-white/[.06]">
-              src/app/page.tsx
-            </code>
-            .
+            Get started by editing <InlineCode>src/app/page.tsx</InlineCode>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
